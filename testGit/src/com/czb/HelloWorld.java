810e9827e09ce38c0adebcd60361d6f08594b27a
zbchen <1126232755@qq.com>
2017-02-24 16:43:30 +0800

    第二次修改

--- testGit/src/com/czb/HelloWorld.java
+++ testGit/src/com/czb/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String args[]){
 		System.out.println("hello world,hello git");
 		System.out.println("第一次修改");
+		System.out.println("第二次修改");
 	}
 
 }
